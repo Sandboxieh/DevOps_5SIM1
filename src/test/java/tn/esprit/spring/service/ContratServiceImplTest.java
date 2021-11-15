d4799f7372e1e2e3714f00680ada1d3ee7d49a2f
thedocf <49178382+thedocf@users.noreply.github.com>
2021-11-15 01:14:53 +0100

    Update ContratServiceImplTest.java

--- src/test/java/tn/esprit/spring/service/ContratServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/ContratServiceImplTest.java
@@ -64,8 +64,9 @@ public class ContratServiceImplTest {
 			Contrat ct = cs.retrieveContratByID((long) 1);
 			ct.setTypeContrat("UPDATED");
 			ct.setDateDebutContrat(new java.util.Date());
+			List<Contrat> listContrats =cs.retrieveAllContrats();
 			Contrat cc =listContrats.get(listContrats.size()-1);
-			ct.setSalaire((cc.getReferenceContrat()-1)*100);
+			ct.setSalaire((cc.getReferenceContrat()-1)*1000);
 			Contrat u_saved=cs.updateContrat(ct);
 			Assertions.assertNotEquals(ct,u_saved);
 			
